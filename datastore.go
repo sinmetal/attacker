914b580728f1d588e6a000bd9a990e92cb6d642e
sinmetal <metal.tie@gmail.com>
2018-08-15 20:16:07 +0900

    Index貼ってないEntityのPutを追加した refs #1

--- datastore.go
+++ datastore.go
@@ -9,6 +9,24 @@ import (
 	"go.opencensus.io/trace"
 )
 
+// LightEntity is Single Property Indexを貼ってないEntity
+type LightEntity struct {
+	I          int       `datastore:",noindex"`
+	J          int       `datastore:",noindex"`
+	K          float64   `datastore:",noindex"`
+	Order1     int64     `datastore:",noindex"`
+	Order2     int64     `datastore:",noindex"`
+	Order3     int64     `datastore:",noindex"`
+	Order4     int64     `datastore:",noindex"`
+	Order5     int64     `datastore:",noindex"`
+	CreatedAt1 time.Time `datastore:",noindex"`
+	CreatedAt2 time.Time `datastore:",noindex"`
+	CreatedAt3 time.Time `datastore:",noindex"`
+	CreatedAt4 time.Time `datastore:",noindex"`
+	CreatedAt5 time.Time `datastore:",noindex"`
+}
+
+// HeavyEntity is 単調増加するSingle Property Indexをがっつり貼ってるEntity
 type HeavyEntity struct {
 	I          int
 	J          int
@@ -25,10 +43,12 @@ type HeavyEntity struct {
 	CreatedAt5 time.Time
 }
 
+// Datastore is Datastore Client struct
 type Datastore struct {
 	DS *datastore.Client
 }
 
+// NewDatastore is Datastore Client 作成
 func NewDatastore(projectID string) (*Datastore, error) {
 	ctx := context.Background()
 
@@ -41,6 +61,7 @@ func NewDatastore(projectID string) (*Datastore, error) {
 	}, nil
 }
 
+// PutHeavyEntity is HeavyEntity put to Datastore
 func (ds *Datastore) PutHeavyEntity(ctx context.Context, i int, j int, k float64) error {
 	ctx, span := trace.StartSpan(ctx, "/PutHeavyEntity")
 	defer span.End()
@@ -63,6 +84,33 @@ func (ds *Datastore) PutHeavyEntity(ctx context.Context, i int, j int, k float64
 		CreatedAt5: now,
 	}
 
-	_, err := ds.DS.Put(ctx, datastore.NameKey("Heavy1", id, nil), e)
+	_, err := ds.DS.Put(ctx, datastore.NameKey("Heavy3", id, nil), e)
+	return err
+}
+
+// PutLightEntity is LightEntity put to Datastore
+func (ds *Datastore) PutLightEntity(ctx context.Context, i int, j int, k float64) error {
+	ctx, span := trace.StartSpan(ctx, "/PutLightEntity")
+	defer span.End()
+	id := uuid.New().String()
+
+	now := time.Now()
+	e := &LightEntity{
+		I:          i,
+		J:          j,
+		K:          k,
+		Order1:     now.Unix(),
+		Order2:     now.Unix(),
+		Order3:     now.Unix(),
+		Order4:     now.Unix(),
+		Order5:     now.Unix(),
+		CreatedAt1: now,
+		CreatedAt2: now,
+		CreatedAt3: now,
+		CreatedAt4: now,
+		CreatedAt5: now,
+	}
+
+	_, err := ds.DS.Put(ctx, datastore.NameKey("Light3", id, nil), e)
 	return err
 }
--- main.go
+++ main.go
@@ -19,12 +19,19 @@ var ds *Datastore
 
 func main() {
 	var projectID string
+	var task string
 	if metadata.OnGCE() {
 		p, err := metadata.ProjectID()
 		if err != nil {
 			panic(err)
 		}
 		projectID = p
+
+		t, err := metadata.InstanceAttributeValue("task")
+		if err != nil {
+			panic(err)
+		}
+		task = t
 	}
 	p := os.Getenv("GOOGLE_CLOUD_PROJECT")
 	fmt.Printf("Env GOOGLE_CLOUD_PROJECT:%s\n", p)
@@ -32,7 +39,14 @@ func main() {
 		projectID = p
 	}
 
+	t := os.Getenv("ATTACKER_TASK")
+	fmt.Printf("Env ATTACKER_TASK:%s\n", t)
+	if len(t) > 0 {
+		task = t
+	}
+
 	fmt.Printf("ProjectID is %s\n", projectID)
+	fmt.Printf("Task is %s\n", task)
 
 	{
 		exporter, err := stackdriver.NewExporter(stackdriver.Options{
@@ -52,7 +66,14 @@ func main() {
 		}
 	}
 
-	run(sample)
+	switch task {
+	case "DATASTORE_PutHeavyEntity":
+		run(runDatastorePutHeavyEntity)
+	case "DATASTORE_PutLightEntity":
+		run(runDatastorePutLightEntity)
+	default:
+		fmt.Printf("%s is not found task", task)
+	}
 }
 
 func run(f func(i int, j int, k float64) error) {
@@ -73,7 +94,7 @@ func run(f func(i int, j int, k float64) error) {
 					case <-t.C:
 						defer wg.Done()
 						fmt.Printf("Start %d,%d,%f\n", i, j, k)
-						if err := runDatastore(i, j, k); err != nil {
+						if err := f(i, j, k); err != nil {
 							fmt.Printf("Faile %d,%d,%f : %+v\n", i, j, k, err)
 						}
 					}
@@ -95,7 +116,12 @@ func sample(i int, j int, k float64) error {
 	return nil
 }
 
-func runDatastore(i int, j int, k float64) error {
+func runDatastorePutHeavyEntity(i int, j int, k float64) error {
 	ctx := context.Background()
 	return ds.PutHeavyEntity(ctx, i, j, k)
+}
+
+func runDatastorePutLightEntity(i int, j int, k float64) error {
+	ctx := context.Background()
+	return ds.PutLightEntity(ctx, i, j, k)
 }
